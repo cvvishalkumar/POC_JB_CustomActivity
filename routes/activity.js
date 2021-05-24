3732dfb71ea3120dbacb7707c28e9d873151fe5d
cvvishalkumar <cvvishalkumar@gmail.com>
2021-05-24 15:56:24 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -86,7 +86,7 @@ exports.execute = function (req, res) {
         if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
             
             // decoded in arguments
-            var decodedArgs = decoded.inArguments[0];
+            var decodedArgs = decoded.inArguments[1];
 
             var options = {
               method: "POST",
