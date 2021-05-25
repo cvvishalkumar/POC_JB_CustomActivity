b610a5aa3f93a05849e1bed2cc260547e6b1187b
cvvishalkumar <cvvishalkumar@gmail.com>
2021-05-25 11:20:05 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -107,7 +107,7 @@ exports.execute = function (req, res) {
                         
             /*Sending inArguments to a cloud page*/
             var request = require('request');
-            var url = 'https://fe3515717564047a771c77.pub.s11.sfmc-content.com/zw2ymoehwew';
+            var url = 'https://fe3515717564047a771c77.pub.s11.sfmc-content.com/ixnlcqxhnv1';
             //var url = 'https://enivnrwna7qqdm7.m.pipedream.net'
             request({
                         url:url,
